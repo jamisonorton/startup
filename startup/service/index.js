109209c271ca16cfc94c919e945c6dc9ff695e35
Jamison Orton <jorton4@byu.edu>
2024-12-05 16:49:01 -0700

    test

--- startup/service/index.js
+++ startup/service/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+
 import { connectToDatabase } from "./database.js";
 import authRouter from "./routes/auth.js"; // Import the auth router
 
