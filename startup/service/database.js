d8af61ea0daf4e30a5be001465fb31f86c94d410
Jamison Orton <jorton4@byu.edu>
2024-12-06 06:47:27 -0700

    database connected

--- startup/service/database.js
+++ startup/service/database.js
@@ -1,21 +1,8 @@
-import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
-import authRoutes from "./auth.js";
-
 dotenv.config({ path: "../.env" });
 
-const app = express();
-const port = process.env.PORT || 5000;
-
-// Middleware
-app.use(express.json());
-
-// Routes
-app.use("/api/auth", authRoutes);
-
-// Connect to MongoDB
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
@@ -26,8 +13,4 @@ const connectDB = async () => {
   }
 };
 
-connectDB();
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+export default connectDB;
