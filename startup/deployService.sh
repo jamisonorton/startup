014722641df523fe8e4f936d1180395446720073
Jamison Orton <jorton4@byu.edu>
2024-12-06 10:44:23 -0700

    test

--- startup/deployService.sh
+++ startup/deployService.sh
@@ -23,7 +23,7 @@ npm install # make sure vite is installed so that we can bundle
 npm run build # build the React front end
 cp -rf dist build/public # move the React front end to the target distribution
 cp service/*.js build # move the back end service to the target distribution
-cp service/*.json build
+cp *.json build
 
 # Step 2
 printf "\n----> Clearing out previous distribution on the target\n"
