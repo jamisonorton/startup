232c45fdc7ae401ced9e1e90a061d70fa9894290
Jamison Orton <jorton4@byu.edu>
2024-12-06 10:18:48 -0700

    test

--- startup/service/index.js
+++ startup/service/index.js
@@ -9,6 +9,15 @@ const port = process.env.PORT;
 // Middleware
 app.use(express.json());
 
+// Use the cookie parser middleware for tracking authentication tokens
+app.use(cookieParser());
+
+// Serve up the applications static content
+app.use(express.static("public"));
+
+// Trust headers that are forwarded from the proxy so we can determine IP addresses
+app.set("trust proxy", true);
+
 // Routes
 app.use("/api/auth", authRoutes);
 
