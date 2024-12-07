2a3a17b3ee536d70ab03b0f4dae2a97e527a2849
Jamison Orton <jorton4@byu.edu>
2024-12-07 13:33:02 -0700

    updated the database to be janesmusicstudio with a collection user

--- startup/service/database.js
+++ startup/service/database.js
@@ -6,8 +6,8 @@ const config = require("./dbConfig.json");
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
-const db = client.db("users");
-const userCollection = db.collection("users");
+const db = client.db("janesmusicstudio");
+const userCollection = db.collection("user");
 
 // This will asynchronously test the connection and exit the process if it fails
 (async function testConnection() {
